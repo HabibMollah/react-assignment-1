345c55a1eee97fdffef58555753595af31ec93a8
HabibMollah <habib.prodev@gmail.com>
2023-10-02 01:34:34 +0600

    finish navbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import NavBar from "./components/NavBar";
+
 export default function App() {
-  return <div className="bg-red-600 text-9xl">App</div>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
