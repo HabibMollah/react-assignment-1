c55d3aa7cf148521466b16f9ea9a1c6745fcd7c1
HabibMollah <habib.prodev@gmail.com>
2023-10-02 12:54:34 +0600

    create context for search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
 import { CiSearch } from "react-icons/ci";
+import useSearchContext from "../hooks/useSearchContext";
 
 export default function NavBar() {
+  const { searchValue, setSearchValue } = useSearchContext();
   return (
     <header className="mx-auto mt-2 flex h-[90px] max-w-[1200px] items-center rounded-3xl bg-accent pl-6 font-saira shadow-md">
       <nav className="flex gap-6">
         <span className="relative">
           <input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
             className="h-12 w-[400px] rounded-2xl py-2 pl-4 pr-12"
             type="text"
             placeholder="Search..."
