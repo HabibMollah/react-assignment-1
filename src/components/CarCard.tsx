1872138a2b505e7d6a1ccabec9f3cd3a4c674bce
HabibMollah <habib.prodev@gmail.com>
2023-10-02 09:38:26 +0600

    finish car card design

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,3 +1,9 @@
+import { AiOutlineHeart } from "react-icons/ai";
+import { LuUsers2 } from "react-icons/lu";
+import { LiaGasPumpSolid } from "react-icons/lia";
+import { BiTachometer } from "react-icons/bi";
+import { RiSteering2Line } from "react-icons/ri";
+
 type Props = {
   car: {
     id: number;
@@ -14,13 +20,58 @@ type Props = {
 
 export default function CarCard({ car }: Props) {
   return (
-    <div className="">
-      <img
-        className="h-[240px] w-[350px] rounded-2xl object-cover"
-        src={car.imageURL}
-        alt={"image of " + car.name}
-      />
-      <h3>{car.name}</h3>
+    <div className="rounded-2xl p-3 shadow-[2px_4px_9px_1px_#00000024]">
+      {/* Image */}
+      <span className="block h-[240px] w-[350px] overflow-hidden rounded-2xl">
+        <img
+          className="w-full scale-125"
+          src={car.imageURL}
+          alt={"image of " + car.name}
+        />
+      </span>
+
+      {/* Details */}
+      <div className="px-2 pt-4">
+        {/* Title */}
+        <div className="flex items-center justify-between">
+          <h3 className="font-saira text-2xl">{car.name}</h3>
+          <p className="rounded-xl border-[2px] border-dashed border-primary/50 px-[10px] py-[1px]">
+            {car.released}
+          </p>
+        </div>
+        {/* Specifications */}
+        <div className="grid grid-cols-2 gap-2 pt-4 font-saira">
+          <p className="flex items-center gap-1">
+            <LuUsers2 className="h-5 w-5 !text-primary" />
+            {car.seats} People
+          </p>
+          <p className="flex items-center gap-1">
+            <LiaGasPumpSolid className="h-5 w-5 !text-primary" /> {car.fuel}
+          </p>
+          <p className="flex items-center gap-1">
+            <BiTachometer className="h-5 w-5 !text-primary" />
+            {car.mileage}
+          </p>
+          <p className="flex items-center gap-1">
+            <RiSteering2Line className="h-5 w-5 !text-primary" />
+            {car.transmission}
+          </p>
+        </div>
+        {/* Rent */}
+        <div className="mt-4 flex items-center justify-between border-t-2 pt-4">
+          <div className="font-saira text-2xl">
+            ${car.rentPerMonth} <span className="text-base">/ month</span>
+          </div>
+          <div className="flex items-center gap-3 py-2">
+            <button className="flex items-center">
+              <AiOutlineHeart className="block h-9 w-9 rounded-lg bg-[#dbecfb] p-[6px] !text-primary/60  " />
+            </button>
+            <button className="text-white rounded-xl bg-primary px-4 py-2 text-sm tracking-tight">
+              Rent now
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -5,8 +5,8 @@ export default function CarList() {
   const cars = data.cars;
 
   return (
-    <main className="mx-auto max-w-[1200px]">
-      <section className="grid grid-cols-3 justify-items-center">
+    <main className="mx-auto mt-4 max-w-[1200px]">
+      <section className="grid grid-cols-3 justify-items-center gap-8">
         {cars.map((car) => (
           <CarCard key={car.id} car={car} />
         ))}
