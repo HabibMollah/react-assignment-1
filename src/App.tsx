75dc2652826467745874902827b75858a95dccdf
HabibMollah <habib.prodev@gmail.com>
2023-10-02 10:04:06 +0600

    refactor layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import NavBar from "./components/NavBar";
 export default function App() {
   return (
     <>
-      <NavBar />
-      <CarList />
+      <header className="mx-auto mt-2 flex h-[90px] max-w-[1200px] items-center rounded-3xl bg-accent pl-6 font-saira shadow-md">
+        <NavBar />
+      </header>
+      <main className="mx-auto mt-8 max-w-[1200px]">
+        <CarList />
+      </main>
     </>
   );
 }
--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -5,12 +5,10 @@ export default function CarList() {
   const cars = data.cars;
 
   return (
-    <main className="mx-auto mt-4 max-w-[1200px]">
-      <section className="grid grid-cols-3 justify-items-center gap-8">
-        {cars.map((car) => (
-          <CarCard key={car.id} car={car} />
-        ))}
-      </section>
-    </main>
+    <section className="grid grid-cols-3 justify-items-center gap-8">
+      {cars.map((car) => (
+        <CarCard key={car.id} car={car} />
+      ))}
+    </section>
   );
 }
